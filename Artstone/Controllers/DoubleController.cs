5e95c240a0bd63f5d7bcf8b894c4288c80858138
oleg shyshka <ww4ka@ukr.net>
2015-07-02 00:31:36 +0300

    add lightbox as plugin to show images
    Signed-off-by: oleg shyshka <ww4ka@ukr.net>

---
+++ Artstone/Controllers/DoubleController.cs
@@ -0,0 +1,26 @@
+﻿using Artstone.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Artstone.Controllers
+{
+    public class DoubleController : Controller
+    {
+        //
+        // GET: /Double/
+
+        public ActionResult Index()
+        {
+            var model = new SingleModel();
+            string[] lstPhotos = Directory.GetFiles(Server.MapPath("~/Images/Single"));
+            foreach (var item in lstPhotos)
+                model.SliderPictures.Add(new Monument(item));
+            return View(model);
+        }
+
+    }
+}
